de3a19ce5730ea0de96219147692810615f6d305
nguyenducde <ducde678@gmail.com>
2020-10-18 09:38:34 +0700

    product_Search_v0

--- src/app/pages/products/products.page.ts
+++ src/app/pages/products/products.page.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProductService } from '../../services/product-service';
 import { AuthenticationService } from '../../services/authentication-service';
+import { IonSearchbar } from '@ionic/angular';
 @Component({
   selector: 'app-products',
   templateUrl: './products.page.html',
   styleUrls: ['./products.page.scss'],
 })
 export class ProductsPage implements OnInit {
+  @ViewChild('search',{static:false}) search:IonSearchbar;
   currentImage: any;
   products;
   productTypes;
@@ -19,6 +21,22 @@ export class ProductsPage implements OnInit {
   }
 
   ngOnInit() {
+  }
+  ionViewDidEnter() {
+    setTimeout(() => {
+      this.search.setFocus();
+    })
+  }
+  _searchChange(event)
+  {
+    const val=event.target.value;
+    if(val&&val.trim()!='')
+    { 
+      this.products=this.products.filter((item:any) => {
+        return (item.name.toLowerCase().indexOf(val.toLowerCase())> -1);
+      })
+    }
+
   }
 
 }
