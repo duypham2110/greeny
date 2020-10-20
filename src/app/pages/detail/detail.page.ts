4fc72b65f7edc072260f753e6e4b58d3eefc2eb6
Duy <duy211099@gmail.com>
2020-10-20 16:20:02 +0700

    save cart to localstorage dit me cuoc doi

--- src/app/pages/detail/detail.page.ts
+++ src/app/pages/detail/detail.page.ts
@@ -12,20 +12,25 @@ import { Product } from '../../models/product';
   styleUrls: ['./detail.page.scss'],
 })
 export class DetailPage implements OnInit {
+  user: any;
   loadedProduct: any;
+  $key: any;
   constructor(
     //Dependency Injection
     public route: Router,
     private authService: AuthenticationService,
     private activatedRoute: ActivatedRoute,
     private pdService: ProductService,
-    
+
   ) { }
 
+  
   ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('user'));
+
     this.activatedRoute.queryParams.subscribe(paramMap => {
-      const $key = paramMap.$key;
-      let temp = this.pdService.getProduct($key);
+      this.$key = paramMap.$key;
+      let temp = this.pdService.getProduct(this.$key);
       temp.snapshotChanges().subscribe(item => {
         {
           this.loadedProduct = item.payload.toJSON();
@@ -36,5 +41,30 @@ export class DetailPage implements OnInit {
 
   openProducer() {
     this.route.navigate(['tabs/producer']);
+  }
+
+  // Them vo localStorage
+  addToCart() {
+    let getItemLocalStorage = JSON.parse(localStorage.getItem(this.user.uid));
+
+    if (getItemLocalStorage === null) {
+      getItemLocalStorage = {
+        products: [
+
+        ]
+      }
+    }
+
+    let item =getItemLocalStorage.products.find(item => item.id == this.$key);
+
+    if(item) {
+      item.quantity+=1;
+    } else {
+      getItemLocalStorage.products.push({ id: this.$key, quantity: 1 });
+    }
+
+    console.log(getItemLocalStorage);
+
+    localStorage.setItem(this.user.uid, JSON.stringify(getItemLocalStorage));
   }
 }
