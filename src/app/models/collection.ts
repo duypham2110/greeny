e39fcdf115b28d4034d86f2c10944ca4f505cf33
duchieu <duchieu.ctk41@gmail.com>
2020-11-27 21:01:24 +0700

    [1.0.0] fix fault conflix.

--- src/app/models/collection.ts
+++
@@ -1,6 +0,0 @@
-import { Product } from './product';
-
-export interface Collection{
-    name:string;
-    products:Product[]
-}
--- src/app/models/product.ts
+++
@@ -1,9 +0,0 @@
-export class Product{
-    $key: string;
-    name: string;
-    productType: string;
-    price: number;
-    description: string;
-    madeIn: string;
-    images: string[];
-}
